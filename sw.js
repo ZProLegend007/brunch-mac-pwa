3aeb77380e4da34724178ad2967b018ea39fec45
ZProLegend007 <97338659+ZProLegend007@users.noreply.github.com>
2023-10-28 21:06:44 +0800

    Update and customize brunch-mac-pwa

--- sw.js
+++ sw.js
@@ -5,26 +5,26 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('v1').then(function(cache) {
 			return cache.addAll([
-				'/brunch-pwa/',
-				'/brunch-pwa/index.html',
-				'/brunch-pwa/css/style.css',
-				'/brunch-pwa/html/addons.html',
-				'/brunch-pwa/html/chromeos.html',
-				'/brunch-pwa/html/settings.html',
-				'/brunch-pwa/images/background/background.png',
-				'/brunch-pwa/images/icons/48.png',
-				'/brunch-pwa/images/icons/96.png',
-				'/brunch-pwa/images/icons/144.png',
-				'/brunch-pwa/images/icons/192.png',
-				'/brunch-pwa/images/icons/256.png',
-				'/brunch-pwa/images/icons/384.png',
-				'/brunch-pwa/images/icons/512.png',
-				'/brunch-pwa/js/addons.js',
-				'/brunch-pwa/js/brunch.js',
-				'/brunch-pwa/js/chromeos.js',
-				'/brunch-pwa/js/cookie.js',
-				'/brunch-pwa/js/settings.js',
-				'/brunch-pwa/js/ws.js'
+				'/brunch-mac-pwa/',
+				'/brunch-mac-pwa/index.html',
+				'/brunch-mac-pwa/css/style.css',
+				'/brunch-mac-pwa/html/addons.html',
+				'/brunch-mac-pwa/html/chromeos.html',
+				'/brunch-mac-pwa/html/settings.html',
+				'/brunch-mac-pwa/images/background/background.png',
+				'/brunch-mac-pwa/images/icons/48.png',
+				'/brunch-mac-pwa/images/icons/96.png',
+				'/brunch-mac-pwa/images/icons/144.png',
+				'/brunch-mac-pwa/images/icons/192.png',
+				'/brunch-mac-pwa/images/icons/256.png',
+				'/brunch-mac-pwa/images/icons/384.png',
+				'/brunch-mac-pwa/images/icons/512.png',
+				'/brunch-mac-pwa/js/addons.js',
+				'/brunch-mac-pwa/js/brunch.js',
+				'/brunch-mac-pwa/js/chromeos.js',
+				'/brunch-mac-pwa/js/cookie.js',
+				'/brunch-mac-pwa/js/settings.js',
+				'/brunch-mac-pwa/js/ws.js'
 			]);
 		})
 	);
@@ -41,7 +41,7 @@ self.addEventListener('fetch', function(event) {
 const getversion = async () => {
 	console.log('In periodicsync handler');
 	ws_connect();
-	setTimeout(() => { ws.send("latest-stable\nlatest-unstable\nlatest-chromeos"); }, 2000);
+	setTimeout(() => { ws.send("latest\nlatest-chromeos"); }, 2000);
 };
 
 self.addEventListener('periodicsync', event => {
@@ -53,12 +53,12 @@ self.addEventListener('periodicsync', event => {
 
 self.addEventListener('notificationclick', function (event) {
 	console.log(event.notification.data.tab);
-	const rootUrl = new URL('/brunch-pwa/', location).href;
+	const rootUrl = new URL('/brunch-mac-pwa/', location).href;
 	var targetUrl;
-	if (event.notification.data.tab === "brunch") {
-		targetUrl = new URL('/brunch-pwa/', location).href;
+	if (event.notification.data.tab === "brunch-mac") {
+		targetUrl = new URL('/brunch-mac-pwa/', location).href;
 	} else {
-		targetUrl = new URL('/brunch-pwa/html/' + event.notification.data.tab + '.html', location).href;
+		targetUrl = new URL('/brunch-mac-pwa/html/' + event.notification.data.tab + '.html', location).href;
 	}
 	event.notification.close();
 	event.waitUntil(
