30e514ddbcaeac88fdf66697845f37e2aa880b0b
Zac <97338659+ZProLegend007@users.noreply.github.com>
2023-10-31 20:09:51 +0800

    Update brunch.js

--- js/brunch.js
+++ js/brunch.js
@@ -1,117 +1,97 @@
 window.onload = function () {
-    if (window["WebSocket"]) {
-        ws_connect();
-        ws.onclose = function (evt) {
-            document.body.innerHTML = '<div style="direction: ltr; position: fixed; top: 0; z-index: 999999; display: block; width: 100%; height: 100%; background: #04041a"><p style="position: relative; top: 40%; display: block; font-size: 32px; font-weight: bold; color: #fff; margin: 0 auto; text-align: center">Connecting to your device.<br>If this screen stays for longer than a brief period, please reload or notify ZProLegend007.</p></div>';
-        };
-    } else {
-        document.body.innerHTML = '<div style="direction: ltr; position: fixed; top: 0; z-index: 999999; display: block; width: 100%; height: 100%; background: #04041a"><p style="position: relative; top: 40%; display: block; font-size: 32px; font-weight: bold; color: #fff; margin: 0 auto; text-align: center">Connecting to your device.<br>If this screen stays for longer than a brief period, please reload or notify ZProLegend007.</p></div>';
-    }
 
-    checkCookie();
+	if (window["WebSocket"]) {
+		ws_connect();
+		ws.onclose = function (evt) {
+			document.body.innerHTML = '<div style="direction: ltr; position: fixed; top: 0; z-index: 999999; display: block; width: 100%; height: 100%; background: #04041a"><p style="position: relative; top: 40%; display: block; font-size: 32px; font-weight: bold; color: #fff; margin: 0 auto; text-align: center">Connecting to your device.<br>If this screen stays for longer than a brief period please reload or notify ZProLegend007.</p></div>';
+		};
+	} else {
+		document.body.innerHTML = '<div style="direction: ltr; position: fixed; top: 0; z-index: 999999; display: block; width: 100%; height: 100%; background: #04041a"><p style="position: relative; top: 40%; display: block; font-size: 32px; font-weight: bold; color: #fff; margin: 0 auto; text-align: center">Connecting to your device.<br>If this screen stays for longer than a brief period please reload or notify ZProLegend007.</p></div>';
+	}
 
-    window.addEventListener('appinstalled', () => {
-        window.location.reload();
-    });
+	checkCookie();
+	
+	window.addEventListener('appinstalled', () => {
+		window.location.reload();
+	});
 
-    async function periodicsync() {
-        const registration = await navigator.serviceWorker.ready;
-        try {
-            await registration.periodicSync.register('get-latest-version', {
-                minInterval: 12 * 60 * 60 * 1000,
-            });
-        } catch {
-            console.log('Periodic Sync could not be registered!');
-            return;
-        }
-        if (Notification.permission !== "granted") {
-            if (await Notification.requestPermission() === "granted") {
-                setCookie("notifications", "yes");
-                showNotification("Only brunch-mac update notifications are enabled by default. You can add more in the settings tab.", "brunch-mac");
-                return;
-            }
-            console.log("Notifications disabled by the user");
-            setCookie("notifications", "no");
-            await registration.periodicSync.unregister('get-latest-version');
-        }
-    }
+	async function periodicsync() {
+		const registration = await navigator.serviceWorker.ready;
+		try {
+			await registration.periodicSync.register('get-latest-version', {
+				minInterval: 12 * 60 * 60 * 1000,
+			});
+		} catch {
+			console.log('Periodic Sync could not be registered!');
+			return;
+		}
+		if (Notification.permission !== "granted") {
+			if (await Notification.requestPermission() === "granted") {
+				setCookie("notifications", "yes");
+				showNotification("Only brunch-mac update notifications are enabled by default. You can add more in the settings tab.", "brunch-mac");
+				return;
+			}
+			console.log("Notifications disabled by user");
+			setCookie("notifications", "no");
+			await registration.periodicSync.unregister('get-latest-version');
+		}
+	}
 
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/brunch-mac-pwa/sw.js', { scope: '/brunch-mac-pwa/' }).then(function (reg) {
-            console.log('Registration succeeded. Scope is ' + reg.scope);
-            periodicsync();
-        }).catch(function (error) {
-            console.log('Registration failed with ' + error);
-        });
-    }
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/brunch-mac-pwa/sw.js', {scope: '/brunch-mac-pwa/'}).then(function(reg) {
+			console.log('Registration succeeded. Scope is ' + reg.scope);
+			periodicsync();
+		}).catch(function(error) {
+			console.log('Registration failed with ' + error);
+		});
+	};
 
-    refresh_data = function () {
-        document.getElementById("log").innerHTML = log;
-    };
+	refresh_data = function() {
+		document.getElementById("log").innerHTML = log;
+	};
+	
+	cookieStore.addEventListener('change', event => {
+  		console.log(`${event.changed.length} changed cookies`);
+  		for (const cookie of event.changed) {
+    			console.log(`Cookie ${cookie.name} changed to ${cookie.value}`);
+			if (cookie.value) {
+				switch (cookie.name) {
+				  case "brunch_version":
+					document.getElementById("brunch-mac-version").innerHTML = '<p>Currently installed brunch-mac file:</p><br>'+cookie.value;
+					break;
+				  case "latest_stable":
+					document.getElementById("latest").innerHTML = '<b>Latest Brunch-mac:</b><br>'+cookie.value+'<br> <a href="https://github.com/zprolegend007/brunch-mac/releases/latest" target="_blank"></a>' ;
+					document.getElementById("form").innerHTML = '<button type="submit" class="buttonstyle">Install the latest brunch-mac</button>';
+					break;
+				}
+			}
+		}
 
-    cookieStore.addEventListener('change', event => {
-        console.log(`${event.changed.length} changed cookies`);
-        for (const cookie of event.changed) {
-            console.log(`Cookie ${cookie.name} changed to ${cookie.value}`);
-                switch (cookie.name) {
-                    case "brunch_version":
-                        document.getElementById("brunch-mac-version").innerHTML = '<p>Currently installed brunch-mac file:</p><br>' + cookie.value;
-                        break;
-                    case "latest_stable":
-                        document.getElementById("latest").innerHTML = '<b>Latest Brunch-mac:</b><br>' + cookie.value + '<br> <a href="https://github.com/zprolegend007/brunch-mac/releases/latest" target="_blank"></a>';
-                        document.getElementById("form").innerHTML = '<button type="submit" class="buttonstyle">Install the latest brunch-mac</button>';
-                        break;
-                }
-            }
-        });
+  		console.log(`${event.deleted.length} deleted cookies`);
+ 		for (const cookie of event.deleted) {
+			console.log(`Cookie ${cookie.name} deleted`);
+			switch (cookie.name) {
+			  case "brunch_version":
+				document.getElementById("brunch-mac-version").innerHTML = '';
+				break;
+			  case "latest_stable":
+				document.getElementById("latest").innerHTML = '';
+				document.getElementById("form").innerHTML = '';
+				break;
+			}
+		}
+	});
+	
+	document.getElementById("form").onsubmit = function () {
+		document.getElementById("log").style.background = " #191e2e";
+		log = "<center><b>Console log:</b></center><br>";
+		document.getElementById("log").innerHTML = log;
+		if (!ws) {
+			return false;
+		}
+		ws.send("update");
+		return false;
+	};
 
-        console.log(`${event.deleted.length} deleted cookies`);
-        for (const cookie of event.deleted) {
-            console.log(`Cookie ${cookie.name} deleted`);
-            switch (cookie.name) {
-                case "brunch_version":
-                    document.getElementById("brunch-mac-version").innerHTML = '';
-                    break;
-                case "latest_stable":
-                    document.getElementById("latest").innerHTML = '';
-                    document.getElementById("form").innerHTML = '';
-                    break;
-            }
-        }
-    }
-
-    document.getElementById("form").onsubmit = function () {
-        document.getElementById("log").style.background = "#191e2e";
-        log = "<center><b>Console log:</b></center><br>";
-        document.getElementById("log").innerHTML = log;
-        if (!ws) {
-            return false;
-        }
-        ws.send("update");
-        return false;
-    };
-
-    // Function to show a notification for the update completion
-    function showUpdateNotification() {
-        if (Notification.permission === "granted") {
-            const notification = new Notification("Almost finished!", {
-                body: "You will need to reboot to finish the update. Click below to reboot.",
-            });
-
-            // Add a button to the notification
-            notification.addEventListener("click", function () {
-                const buttonText = "Reboot";
-                // Add your reboot logic here
-                // For example, you can reload the page or execute a reboot command.
-                // window.location.reload(); // Reload the page
-                // Send a message to the PWA's helper script to trigger the reboot.
-                if (ws) {
-                    ws.send("reboot");
-                }
-            });
-        }
-    }
-
-    setTimeout(() => {
-        ws.send("brunch-mac-version\nlatest\nlatest-chromeos");
-    }, 2000);
+	setTimeout(() => { ws.send("brunch-mac-version\nlatest\nlatest-chromeos"); }, 2000);
+};
