40242e33f470385fd242d3c06ead4cb05aa981ce
Zac <97338659+ZProLegend007@users.noreply.github.com>
2023-10-31 20:22:14 +0800

    Update ws.js

--- js/ws.js
+++ js/ws.js
@@ -2,72 +2,130 @@ var ws;
 var log = "";
 
 function refresh_data() {
-	console.log("Data refresh requested without display");
+    console.log("Data refresh requested without display");
 }
 
 function reboot() {
-	ws.send("reboot");
-};
-
-async function showNotification(notification_text, tabname) {
-	const title = 'Brunch-mac PWA';
-	const options = {
-		body: notification_text,
-		icon: '/brunch-mac-pwa/images/icons/512.png',
-		data: {
-			tab: tabname,
-		}
-	};
-	if (typeof Window !== 'undefined') {
-		const sw = await navigator.serviceWorker.ready;
-		sw.showNotification(title, options);
-	} else {
-		self.registration.showNotification(title, options);
-	}
+    ws.send("reboot");
 }
 
+async function showNotification(notification_text, tabname) {
+    const title = 'Brunch-mac PWA';
+    const options = {
+        body: notification_text,
+        icon: '/brunch-mac-pwa/images/icons/144.png',
+        data: {
+            tab: tabname,
+        }
+    };
+    if (typeof Window !== 'undefined') {
+        const sw = await navigator.serviceWorker.ready;
+        sw.showNotification(title, options);
+    } else {
+        self.registration.showNotification(title, options);
+    }
+}
+
+function showUpdateNotification() {
+    if (Notification.permission === "granted") {
+        const notification_text = "You will need to reboot to finish the update. Click below to reboot.";
+        const tabname = "Almost there!"; // You can change the tabname as needed
+
+        const title = 'Brunch-mac PWA';
+        const options = {
+            body: notification_text,
+            icon: '/brunch-mac-pwa/images/icons/512.png',
+            data: {
+                tab: tabname,
+            }
+        };
+
+        // Add a button to the notification
+        options.actions = [
+            { action: 'reboot', title: 'Reboot' }
+        ];
+
+        if (typeof Window !== 'undefined') {
+            const sw = await navigator.serviceWorker.ready;
+            sw.showNotification(title, options).then(function (notification) {
+                // Handle the button click
+                notification.addEventListener("notificationclick", function (event) {
+                    if (event.action === "reboot") {
+                        // Add your reboot logic here
+                        // For example, you can reload the page or execute a reboot command.
+                        // window.location.reload(); // Reload the page
+                        // Send a message to the PWA's helper script to trigger the reboot.
+                        if (ws) {
+                            ws.send("reboot");
+                        }
+                    }
+                });
+            });
+        } else {
+            self.registration.showNotification(title, options).then(function (notification) {
+                // Handle the button click
+                notification.addEventListener("notificationclick", function (event) {
+                    if (event.action === "reboot") {
+                        // Add your reboot logic here
+                        // For example, you can reload the page or execute a reboot command.
+                        // window.location.reload(); // Reload the page
+                        // Send a message to the PWA's helper script to trigger the reboot.
+                        if (ws) {
+                            ws.send("reboot");
+                        }
+                    }
+                });
+            });
+        }
+    }
+}
+
+
 function ws_connect() {
-	ws = new WebSocket("ws://localhost:8080");
-	ws.onclose = function (evt) {
-		console.log("Connection closed");
-	};
-	ws.onmessage = async function (evt) {
-		var notifications = await getCookie("notifications");
-		var brunch_stable = await getCookie("brunch_stable");
-		var latest_stable = await getCookie("latest_stable");
-		var chromeos = await getCookie("chromeos");
-		var latest_chromeos = await getCookie("latest_chromeos");
-		var messages = evt.data.split(':next:');
-		for (var i = 0; i < messages.length; i++) {
-			console.log("Message received: " + messages[i]);
-			if (messages[0] === "brunch-mac-version") {
-				setCookie("brunch_version", messages[1]);
-				break;
-			}
-			if (messages[0] === "latest") {
-				if (notifications.value === "yes" && brunch_stable.value === "yes") {
-					if (latest_stable && latest_stable.value !== "" && messages[1] !== "" && latest_stable.value !== messages[1]) {
-						showNotification("New brunch-mac release available: " + messages[1], "brunch-mac");
-					}
-				}
-				setCookie("latest_stable", messages[1]);
-				break;
-			}
-			if (messages[0] === "chromeos-version") {
-				setCookie("chromeos_version", messages[1]);
-				break;
-			}
-			if (messages[0] === "latest-chromeos") {
-				if (notifications.value === "yes" && chromeos.value === "yes") {
-					if (latest_chromeos && latest_chromeos.value !== "" && messages[1] !== "" && latest_chromeos.value !== messages[1]) {
-						showNotification("New recovery image available: " + messages[1], "chromeos");
-					}
-				}
-				setCookie("latest_chromeos", messages[1]);
-				break;
-			}
-			log += messages[i] + '<br>';
-		}
-		refresh_data();
-	};
+    ws = new WebSocket("ws://localhost:8080");
+    ws.onclose = function (evt) {
+        console.log("Connection closed");
+    };
+    ws.onmessage = async function (evt) {
+        var notifications = await getCookie("notifications");
+        var brunch_stable = await getCookie("brunch_stable");
+        var latest_stable = await getCookie("latest_stable");
+        var chromeos = await getCookie("chromeos");
+        var latest_chromeos = await getCookie("latest_chromeos");
+        var messages = evt.data.split(':next:');
+        for (var i = 0; i < messages.length; i++) {
+            console.log("Message received: " + messages[i]);
+            if (messages[0] === "brunch-mac-version") {
+                setCookie("brunch_version", messages[1]);
+                break;
+            }
+            if (messages[0] === "latest") {
+                if (notifications.value === "yes" && brunch_stable.value === "yes") {
+                    if (latest_stable && latest_stable.value !== "" && messages[1] !== "" && latest_stable.value !== messages[1]) {
+                        showNotification("New brunch-mac release available: " + messages[1], "brunch-mac");
+                    }
+                }
+                setCookie("latest_stable", messages[1]);
+                break;
+            }
+            if (messages[0] === "chromeos-version") {
+                setCookie("chromeos_version", messages[1]);
+                break;
+            }
+            if (messages[0] === "latest-chromeos") {
+                if (notifications.value === "yes" && chromeos.value === "yes") {
+                    if (latest_chromeos && latest_chromeos.value !== "" && messages[1] !== "" && latest_chromeos.value !== messages[1]) {
+                        showNotification("New recovery image available: " + messages[1], "chromeos");
+                    }
+                }
+                setCookie("latest_chromeos", messages[1]);
+                break;
+            }
+            if (messages[0] === "javascript:simulateUpdateCompletion()") {
+                showUpdateNotification();
+            }
+            log += messages[i] + '<br>';
+        }
+        refresh_data();
+    };
 }
