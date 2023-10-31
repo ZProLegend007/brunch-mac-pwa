00a62cfbb6a57d8ab928b652b7faed77732886ff
Zac <97338659+ZProLegend007@users.noreply.github.com>
2023-10-31 11:03:49 +0800

    Update brunch.js

--- js/brunch.js
+++ js/brunch.js
@@ -83,7 +83,7 @@ window.onload = function () {
 	});
 	
 	document.getElementById("form").onsubmit = function () {
-		document.getElementById("log").style.background = "#33266e";
+		document.getElementById("log").style.background = " #191e2e";
 		log = "<center><b>Console log:</b></center><br>";
 		document.getElementById("log").innerHTML = log;
 		if (!ws) {
